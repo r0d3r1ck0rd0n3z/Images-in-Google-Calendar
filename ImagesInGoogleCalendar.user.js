9892431bbc61c6395dbdefe6f34ee8c3e1cb05ad
r0d3r1ck0rd0n3z <360.by.roderick@gmail.com>
2022-12-19 00:21:02 +0800

    Updated to address class change in latest gCal release

--- ImagesInGoogleCalendar.user.js
+++ ImagesInGoogleCalendar.user.js
@@ -3,25 +3,26 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Fix IMG tags imported into Google Calendar
-// @author       https://github.com/r0d3r1ck0rd0n3z
+// @author       Roderick Ordonez
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @match        http*://calendar.google.com/*
 // @include      https://calendar.google.com/*
-// @icon         https://i.ibb.co/BZVSCYG/calendar-01.png
+// @icon         https://www.beyondhousing.org.au/images/favicon/favicon-16x16.png
 // @grant        none
 // ==/UserScript==
 
 document.addEventListener("click", function(){
 
-// Function to fix markup of imported images inserted inside the html-blob tag
+// Function to fix markup of imported images inside the image-blob tag
 
 function replaceIMGs() {
-    var imgs = document.querySelectorAll('html-blob img');
-
+    //var imgs = document.querySelectorAll('html-blob img');
+    var imgs = document.querySelectorAll('img[src*="google.com"]');
     for (var i = 0; i < imgs.length; i++) {
         var newSrc = imgs[i].src.replace('https://www.google.com/url?q=', '').replace(/&sa=D.*/,'');
         imgs[i].setAttribute("style", "width:350px;height:auto;");
         imgs[i].src = decodeURIComponent(newSrc);
+        //console.log(imgs[i].src);
     };
   };
 
